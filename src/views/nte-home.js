997b1837b9ac34671e84db1546d94ef6d7af0829
Avinesh <160318098+Avinesh511@users.noreply.github.com>
2024-05-22 14:55:24 +0530

    Commit from TeleportHQ

---
+++ src/views/nte-home.js
@@ -0,0 +1,133 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './nte-home.css'
+
+const NTEHome = (props) => {
+  return (
+    <div className="nte-home-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="nte-home-nte-home">
+        <img
+          alt="eagt11876"
+          src="/eagt11876-2pt-1300h.png"
+          className="nte-home-eagt1"
+        />
+        <img
+          alt="Component51062"
+          src="/component51062-ndt6.svg"
+          className="nte-home-component5"
+        />
+        <div className="nte-home-group2">
+          <div className="nte-home-group1">
+            <span className="nte-home-text">
+              <span>NTE.AI</span>
+            </span>
+            <span className="nte-home-text02">
+              <span>AI Based Enterprise Document Management system</span>
+            </span>
+          </div>
+        </div>
+        <span className="nte-home-text04">
+          <span>NTE.AI</span>
+        </span>
+        <span className="nte-home-text06">
+          <span>Consulting</span>
+        </span>
+        <span className="nte-home-text08">
+          <span>Product</span>
+        </span>
+        <span className="nte-home-text10">
+          <span>Free Tools</span>
+        </span>
+        <div className="nte-home-frame5">
+          <span className="nte-home-text12">
+            <span>Login</span>
+          </span>
+        </div>
+        <img
+          alt="Rectangle61064"
+          src="/rectangle61064-5bi-600h.png"
+          className="nte-home-rectangle6"
+        />
+        <img
+          alt="Rectangle71064"
+          src="/rectangle71064-hacu-200h.png"
+          className="nte-home-rectangle7"
+        />
+        <img
+          alt="Rectangle81065"
+          src="/rectangle81065-q22o-200h.png"
+          className="nte-home-rectangle8"
+        />
+        <img
+          alt="Rectangle111191"
+          src="/rectangle111191-yx2-200h.png"
+          className="nte-home-rectangle11"
+        />
+        <img
+          alt="Rectangle121192"
+          src="/rectangle121192-uln-200h.png"
+          className="nte-home-rectangle12"
+        />
+        <img
+          alt="Rectangle131192"
+          src="/rectangle131192-ar4-200h.png"
+          className="nte-home-rectangle13"
+        />
+        <span className="nte-home-text14">
+          <span>Get in Touch</span>
+        </span>
+        <img
+          alt="Rectangle91065"
+          src="/rectangle91065-tc4p-200h.png"
+          className="nte-home-rectangle9"
+        />
+        <span className="nte-home-text16">
+          <span>Name</span>
+        </span>
+        <span className="nte-home-text18">
+          <span>Company Name</span>
+        </span>
+        <span className="nte-home-text20">
+          <span>Designation</span>
+        </span>
+        <span className="nte-home-text22">
+          <span>Email</span>
+        </span>
+        <span className="nte-home-text24">
+          <span>Mobile No.</span>
+        </span>
+        <span className="nte-home-text26">
+          <span>Requirements</span>
+        </span>
+        <img
+          alt="Rectangle101066"
+          src="/rectangle101066-95r7-200h.png"
+          className="nte-home-rectangle10"
+        />
+        <span className="nte-home-text28">
+          <span>Launch</span>
+        </span>
+        <div className="nte-home-group8">
+          <span className="nte-home-text30">
+            <span>Text holder</span>
+          </span>
+          <span className="nte-home-text32">
+            <span>ouag sfouuo gougoug ddddgf</span>
+          </span>
+          <img
+            alt="Line41192"
+            src="/line41192-dzduz.svg"
+            className="nte-home-line4"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NTEHome
